2342c3673b46bfe26647ad980986237a1be1d3f4
Ayush Mehta <ayushbhaimehta20002@gmail.com>
2022-06-30 03:46:07 +0530

    first commit

---
+++ build/precache-manifest.a784bc9e8e5d300c3cbeee6d56242395.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ccdf669169f5a731c17b93441c3f818",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0751ac552563581fd2c7",
+    "url": "/static/js/2.419013f1.chunk.js"
+  },
+  {
+    "revision": "b81d00f65313809e49c0",
+    "url": "/static/js/main.ac6cb14e.chunk.js"
+  },
+  {
+    "revision": "b1cfa85f861ca69c3b05",
+    "url": "/static/js/runtime-main.941946b3.js"
+  }
+]);
